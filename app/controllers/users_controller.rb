e18f62a33c2ce133e8fc9650a9f4b2e648071df5
shingokawashima <shingokawashima@gmail.com>
2015-09-29 16:31:25 +0000

    Created edit and update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    
     @user = User.new(user_params)
     if @user.save
       flash[:success]="Welcome to the Sample App!"
@@ -17,6 +16,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Saved changes successfully!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   
   private
   
