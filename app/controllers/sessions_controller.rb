83386b3b3af797857addb1215e7371a419886ce2
shingokawashima <shingokawashima@gmail.com>
2015-10-18 03:04:15 +0000

    solve conflict

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
-<<<<<<< HEAD
-
-=======
   
->>>>>>> user-profile
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-<<<<<<< HEAD
-      flash[:info] = "logged in as #{@user.name}"
-=======
       flash[:info] ="logged in as #{@user.name}"
->>>>>>> user-profile
       redirect_to @user
     else
       flash[:danger] = 'invalid email/password combination'
@@ -26,9 +18,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
-<<<<<<< HEAD
-=======
-  
-  
->>>>>>> user-profile
+
 end
